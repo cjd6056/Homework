d6f8b8c01d6f76b009e87afde27dcf8946146c7d
cjd6056 <cjd6056@psu.edu>
2024-11-05 10:41:30 -0500

    C_make debugging

---
+++ HW_2/src/main.cpp
@@ -0,0 +1,27 @@
+#include "matplot.h"  // This line should work if the path is set correctly in CMake
+#include <cmath>
+#include <vector>
+
+int main() {
+    // Define the range and step
+    std::vector<double> x;
+    std::vector<double> y;
+
+    // Generate x values from -2π to 2π
+    for (double i = -2 * M_PI; i <= 2 * M_PI; i += 0.1) {
+        x.push_back(i);
+        y.push_back(std::sin(i));
+    }
+
+    // Create the plot
+    matplot::plot(x, y);
+    matplot::title("Plot of y = sin(x)");
+    matplot::xlabel("x");
+    matplot::ylabel("y");
+    matplot::grid(true);
+
+    // Show the plot
+    matplot::show();
+
+    return 0;
+}
